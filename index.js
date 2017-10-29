2c35640022012f7d0de8f27d7b14be7b82f5d2f6
Erik Laucks <phisigmapi314@gmail.com>
2017-10-29 02:22:27 -0400

    Backend registration

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var path = require('path')
 var fs = require('fs')
 var app = require('express')()
+var bodyParser = require('body-parser')
 // var http = require('http').Server(app)
 var port = process.env.PORT || 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
@@ -17,15 +20,42 @@ app.get('/timing', (req, res) => {
 })
 
 app.get('/api/addCheckpoint/:id', (req, res) => {
-  console.log('received checkpoint with id ' + req.params.id)
-  fs.appendFile(path.join(__dirname, '/checkpoints.csv'), req.params.id + '\n', (err) => { if (err) console.log(err) })
-  fs.appendFile(path.join(__dirname, '/checkpoints_backup.csv'), req.params.id + '\n', (err) => { if (err) console.log(err) })
+  console.log('received checkpoint with id ' + parseInt(req.params.id))
+  fs.appendFile(path.join(__dirname, '/checkpoints.csv'), req.params.id + ',' + (new Date()).toISOString() + '\n', (err) => { if (err) console.log(err) })
+  fs.appendFile(path.join(__dirname, '/checkpoints_backup.csv'), req.params.id + ',' + (new Date()).toISOString() + '\n', (err) => { if (err) console.log(err) })
   res.send('received checkpoint with id ' + req.params.id)
 })
 
-app.get('/api/registerRunner/:name/:type/:id', (req, res) => {
-  console.log('received registration with info ' + req.params.name + ', ' + req.params.type + ', ' + req.params.id)
-  fs.appendFile(path.join(__dirname, '/runners.csv'), req.params.name + ',' + req.params.type + ',' + req.params.id + '\n', (err) => { if (err) console.log(err) })
+app.get('/api/freeCheckpoints', (req, res) => {
+  fs.readFile(path.join(__dirname, '/runners.csv'), 'utf-8', function (err, runners) {
+    if (err) res.send(err.message)
+    else {
+      fs.readFile(path.join(__dirname, '/checkpoints.csv'), 'utf-8', function (e, checkpoints) {
+        runners = runners.split('\n')
+        checkpoints = checkpoints.split('\n')
+        checkpoints = checkpoints.map(function (c) { return c.split(',')[0] })
+        if (e) res.send(e.message)
+        else {
+          runners.forEach(r => {
+            let pieces = r.split(',')
+            if (checkpoints.indexOf(pieces[pieces.length - 1]) >= 0) {
+              checkpoints.splice(checkpoints.indexOf(pieces[pieces.length - 1]), 1)
+            } else {
+              console.log('Runner ' + r + ' has checkpoint not associated with known key.')
+            }
+          })
+          res.send(checkpoints)
+        }
+      })
+    }
+  })
+})
+
+app.post('/api/registerRunner/', (req, res) => {
+  let data = req.body
+  console.log('received registration with info ' + data.name + ', ' + data.type + ', ' + data.heat + ', ' + data.id)
+  fs.appendFile(path.join(__dirname, '/runners.csv'), data.name + ',' + data.type + ',' + data.heat + ',' + data.id + '\n', (err) => { if (err) console.log(err) })
+  fs.appendFile(path.join(__dirname, '/runners_backup.csv'), data.name + ',' + data.type + ',' + data.heat + ',' + data.id + '\n', (err) => { if (err) console.log(err) })
 })
 
 app.get('/api/registerTime/', (req, res) => {
